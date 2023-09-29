b179fff23b903f8b00f48b9a5e719249c84b782c
dnguyen130 <dnguyen130@my.bcit.ca>
2023-09-28 18:33:44 -0700

    color adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ function App() {
     <div>
       <h2 style={{ color: "#65F494" }}>Hello</h2>
       <p style={{ color: "#65F494 " }}>Let's create something together</p>
+      <button>Click Me</button>
     </div>
   );
 }
