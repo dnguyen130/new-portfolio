e67167a4a305cfa7d4dd1247fce4deb73891abc1
dnguyen130 <dnguyen130@my.bcit.ca>
2023-09-29 16:27:57 -0700

    progress

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,7 +1,14 @@
 import { ReactElement } from "react";
+import { MdMenu } from "react-icons/md";
 
 import styles from "@/styles/nav.module.css";
 
 export default function Nav(): ReactElement {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.hamburger}>
+        <MdMenu size="100%" />
+      </div>
+    </div>
+  );
 }
