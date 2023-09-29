60457b6290f4ae4f77cd03e095a32ec313381429
dnguyen130 <dnguyen130@my.bcit.ca>
2023-09-28 18:13:05 -0700

    theme creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import './App.css'
-
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <h2 style={{ color: "#65F494" }}>Hello</h2>
+      <p style={{ color: "#65F494 " }}>Let's create something together</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
