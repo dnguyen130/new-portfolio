9943eae06075e8a55bd4fc9fa62f5d76435f5ced
dnguyen130 <dnguyen130@my.bcit.ca>
2023-09-30 15:28:23 -0700

    hero progress

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,11 +1,42 @@
 import { ReactElement } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
 
 import styles from "./hero.module.css";
+import "swiper/css";
 
 export default function Hero(): ReactElement {
   return (
     <header className={styles.container}>
-      <h1>DANNY NGUYEN</h1>
+      <h1>Danny Nguyen</h1>
+      <h2>I am a</h2>
+      <Swiper
+        modules={[Autoplay]}
+        freeMode
+        spaceBetween={50}
+        slidesPerView={1}
+        loop
+        speed={1000}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: true,
+        }}
+      >
+        <SwiperSlide>
+          <h2>web developer</h2>
+        </SwiperSlide>
+        <SwiperSlide>
+          <h2>front end developer</h2>
+        </SwiperSlide>
+        <SwiperSlide>
+          <h2>back end developer</h2>
+        </SwiperSlide>
+        <SwiperSlide>
+          <h2>full stack developer</h2>
+        </SwiperSlide>
+      </Swiper>
+      <p>Helping you bring your ideas to life.</p>
+      <p>Currently working on projects while seeking career opportunities.</p>
     </header>
   );
 }
